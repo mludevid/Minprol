bdcd3a59bb6eff85f56b5aee810344bc7bebb236
Marc Ludevid Wulf <marc.ludevid@gmail.com>
2020-08-18 20:29:26 +0200

    Added multiple line support.

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ It can handle `+`, `-`, `*`, `/` and `()` for I32 (`1`), I64 (`1l`), F32 (`1.0`)
 To run it you'll need the Rust compiler. You can start the interpreter by running `cargo run`.
 
 Next steps:
-- Reform memory management.
 - Improve TokenCursor: Peak next element instead of step_back and be able to drop past Tokens.
 - If Else
 - Loops
--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     let mut scope = scope::Scope::new();
     loop {
         println!("");
-        let expression = input("minprol> ");    
+        let expression = input("minprol> ", 0, 0);
 
         let tokens = lexical::create_tokens(expression);
 
@@ -30,19 +30,40 @@ fn main() {
     }
 }
 
-fn input(message: &str) -> String {
+fn input(message: &str, parenthesis_count: usize, curly_parenthesis_count: usize) -> String {
     use std::io::{stdin, stdout, Write};
 
     let mut s = String::new();
     print!("{}", message);
 
-    let _=stdout().flush();
+    let _ = stdout().flush();
     stdin().read_line(&mut s).expect("Did not enter a correct string");
-    if let Some('\n')=s.chars().next_back() {
+    if let Some('\n') = s.chars().next_back() {
         s.pop();
     }
-    if let Some('\r')=s.chars().next_back() {
+    if let Some('\r') = s.chars().next_back() {
         s.pop();
+    }
+
+    let parenthesis_count_opening = s.matches("(").count() + parenthesis_count;
+    let curly_parenthesis_count_opening = s.matches("{").count() + curly_parenthesis_count;
+
+    let parenthesis_count_closing = s.matches(")").count();
+    let curly_parenthesis_count_closing = s.matches("}").count();
+
+    if parenthesis_count_opening < parenthesis_count_closing {
+        panic!("Unexpected closing bracket");
+    }
+
+    if curly_parenthesis_count_opening < curly_parenthesis_count_closing {
+        panic!("Unexpected closing curly bracket");
+    }
+
+    let parenthesis = parenthesis_count_opening - parenthesis_count_closing;
+    let curly_parenthesis = curly_parenthesis_count_opening - curly_parenthesis_count_closing;
+
+    if parenthesis > 0 || curly_parenthesis > 0 {
+        s.extend(input(">>>> ", parenthesis, curly_parenthesis).chars());
     }
 
     s
